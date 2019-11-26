907defe37b57665cff4aefd1b0674d5158ab36d2
i.krupnov <i.krupnov@redmadrobot.com>
2019-11-27 02:30:14 +0300

    [Setup] +cors, bodyParser

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import express, {Request, Response} from 'express';
+import express, {Request} from 'express';
 import axios from 'axios';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use((_: Request, res: Response, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/oauth', async (req, res) => {
   res.send(`${req.query.code}, ${req.query.client_id}`);
@@ -18,6 +17,29 @@ app.get('/boards', (_, res) => {
   axios.get('https://api.miro.com/v1/accounts/3074457347044577000/boards', {
     headers: {
       Authorization: 'Bearer d97e2da1-9e08-4abf-8d5e-72d970d1ddd8'
+    }
+  })
+    .then(data => res.send(data.data.data))
+    .catch(error => console.log(error));
+});
+
+type CreateRequest = {
+  name: string;
+};
+app.post('/boards/create', (req: Request, res) => {
+  const {name} = req.body as CreateRequest;
+  axios({
+    url: 'https://api.miro.com/v1/boards',
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer d97e2da1-9e08-4abf-8d5e-72d970d1ddd8'
+    },
+    data: {
+      name,
+      sharingPolicy: {
+        access: 'private',
+        teamAccess: 'private'
+      }
     }
   })
     .then(data => res.send(data.data.data))

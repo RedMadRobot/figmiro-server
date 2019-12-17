42e606ab30174432bff3bc37eb92f10e347b527b
i.krupnov <i.krupnov@redmadrobot.com>
2019-12-17 04:11:33 +0300

    [Pictures] refactor

--- app/modules/pictures/pictures.service.ts
+++ app/modules/pictures/pictures.service.ts
@@ -65,12 +65,12 @@ async function getPictures(dto: CreateOrUpdatePicturesDTO): Promise<Picture[]> {
       ...pic,
       image: pic.image.toString('base64')
     })),
-    (picturesWithFullPath: PictureStringed[]) => picturesWithFullPath.map(pic => ({
+    (picturesBase64: PictureStringed[]) => picturesBase64.map(pic => ({
       ...omit(pic, 'width', 'height'),
       x: parseInt(pic.width, 10) / 2 - parseInt(pic.x, 10),
       y: parseInt(pic.height, 10) / 2 - parseInt(pic.y, 10)
     })),
-    async (picturesBase64: PictureWithProperXY[]) => Promise.all(picturesBase64.map(
+    async (picturesWithProperXY: PictureWithProperXY[]) => Promise.all(picturesWithProperXY.map(
       async (pic, index) => {
         const fileName = `artboard_${dto.boardId}_${uuid()}_${index}.png`;
         const fullPath = path.resolve('./tmp', fileName);

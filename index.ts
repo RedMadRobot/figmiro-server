e3a98f839fccbebe20c2468d2e0c1a591f87f130
i.krupnov <i.krupnov@redmadrobot.com>
2019-12-23 03:41:33 +0300

    [Common] + security

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
 import 'module-alias/register';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import bodyParser from 'body-parser';
-import {boardsController} from 'modules/boards';
-import {authController} from 'modules/auth';
-import {picturesController} from 'modules/pictures';
+import {apiController} from 'modules/api';
 import {mediaController} from 'modules/media';
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(picturesController.root, picturesController.ctx);
-app.use(authController.root, authController.ctx);
-app.use(boardsController.root, boardsController.ctx);
+app.use(apiController.root, apiController.ctx);
 app.use(mediaController.root, mediaController.ctx);
 app.listen(5000);

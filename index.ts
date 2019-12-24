ab592df140592b8da6f4e00e144ee4f2d71abb14
i.krupnov <i.krupnov@redmadrobot.com>
2019-12-24 12:12:15 +0300

    [Common] + up request body size

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(apiController.root, apiController.ctx);
 app.use(mediaController.root, mediaController.ctx);

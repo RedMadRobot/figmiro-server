631e645267a76ab828f038f71e621f27324c2bf3
i.krupnov <i.krupnov@redmadrobot.com>
2020-01-10 11:45:59 +0300

    [Picture] Done with multi

--- app/modules/pictures/pictures.controller.ts
+++ app/modules/pictures/pictures.controller.ts
@@ -2,14 +2,14 @@ import {Request, Response, Router} from 'express';
 import {OK} from 'http-status-codes';
 import {Controller} from 'utils/Controller';
 import {checkUnauthorized, processError} from 'utils/AppError';
-import {createOrUpdatePictures} from './pictures.service';
+import {createOrUpdatePictures, upload} from './pictures.service';
 
 export const picturesController: Controller = {
   root: '/pictures',
   ctx: (() => {
     const ctx = Router();
 
-    ctx.post('/', processCreateOrUpdatePicture);
+    ctx.post('/', upload.array('image'), processCreateOrUpdatePicture);
     async function processCreateOrUpdatePicture(req: Request, res: Response): Promise<void> {
       try {
         checkUnauthorized(req);
--- app/modules/pictures/pictures.service.ts
+++ app/modules/pictures/pictures.service.ts
@@ -1,19 +1,19 @@
 import fs from 'fs-extra';
-import uuid from 'uuid/v1';
+import multer from 'multer';
 import path from 'path';
+import crypto from 'crypto';
 import {INTERNAL_SERVER_ERROR} from 'http-status-codes';
-import {omit} from 'lodash';
 import FormData from 'form-data';
 import {AppError} from 'utils/AppError';
 import {request} from 'utils/request';
 import {RequestWithBody} from 'utils/RequestWithBody';
 import {CreateOrUpdatePicturesDTO, CreateOrUpdatePicturesResponse} from './pictures.dto';
-import {Picture, WidgetWithFigmaId} from './pictures.entity';
+import {Picture, PictureFromClient, WidgetWithFigmaId} from './pictures.entity';
 
 export async function createOrUpdatePictures(
   req: RequestWithBody<CreateOrUpdatePicturesDTO>
 ): Promise<WidgetWithFigmaId[]> {
-  const pictures = await getPictures(req.body);
+  const pictures = getPictures(req.files as Express.Multer.File[], req.body);
   try {
     const formData = new FormData();
     const data = JSON.stringify({
@@ -26,7 +26,7 @@ export async function createOrUpdatePictures(
               x: pic.x,
               y: pic.y
             },
-            ...(req.body.scale ? {scaleData: {scale: 0.5}} : {})
+            ...(req.body.scale === 'true' ? {scaleData: {scale: 0.5}} : {})
           }
         })
       }))
@@ -60,26 +60,46 @@ export async function createOrUpdatePictures(
   }
 }
 
-async function getPictures(dto: CreateOrUpdatePicturesDTO): Promise<Picture[]> {
-  return Promise.all(dto.images.map(
-    async pic => {
-      const fileName = `${pic.name}.png`
-      const imagePath = path.resolve('./tmp', uuid(), fileName);
-      const imageFile = Buffer.from(Object.values(pic.image)).toString('base64');
-      await fs.outputFile(imagePath, imageFile, 'base64');
-      return {
-        ...omit(pic, 'image'),
-        fileName,
-        imagePath
-      };
-    }
-  ));
+function getPictures(
+  images: Express.Multer.File[],
+  dto: CreateOrUpdatePicturesDTO
+): Picture[] {
+  return images.map((image, index) => ({
+    ...JSON.parse(dto.imageMeta[index]) as PictureFromClient,
+    fileName: image.filename,
+    imagePath: image.path
+  }));
 }
 
 async function removeImagesFromTmp(pictures: Picture[]): Promise<void> {
   await Promise.all(
     pictures.map(async pic => {
-      await fs.remove(path.dirname(pic.imagePath));
+      await fs.remove(pic.imagePath);
     })
   );
 }
+
+const TMP_DIR = path.resolve('./tmp');
+export const upload = multer({
+  storage: multer.diskStorage({
+    async destination(
+      _: Express.Request,
+      __: Express.Multer.File,
+      cb: (error: (Error | null), filename: string) => void
+    ): Promise<void> {
+      await fs.ensureDir(TMP_DIR);
+      cb(null, TMP_DIR);
+    },
+    filename(
+      _: Express.Request,
+      file: Express.Multer.File,
+      cb: (error: (Error | null), filename: string) => void
+    ): void {
+      crypto.pseudoRandomBytes(16, (___: Error, raw: Buffer): void => {
+        const extension = path.extname(file.originalname);
+        const filename = path.basename(file.originalname, extension);
+        cb(null, `${filename}__${raw.toString('hex')}__${extension}`);
+      });
+    }
+  })
+});

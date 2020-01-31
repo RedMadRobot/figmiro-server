897f6f402c389427ea3a95a7e87956a11f05563d
i.krupnov <i.krupnov@redmadrobot.com>
2020-01-31 11:13:17 +0300

    [Media] remove media module

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import {apiController} from 'modules/api';
-import {mediaController} from 'modules/media';
 
 const app = express();
 
@@ -13,5 +12,4 @@ app.use(helmet());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(apiController.root, apiController.ctx);
-app.use(mediaController.root, mediaController.ctx);
 app.listen(5000);
